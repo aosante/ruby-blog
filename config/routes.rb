4063e4f2a64ee8cf23a09706b84b5dc07904e7d6
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-06-25 00:50:52 -0600

    add comments model and controller and associate to articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
-  
-  resources :articles
+  # Rails provides a routes method named resources that maps all of the conventional routes for a collection of resources, such as articles
+  resources :articles do # This creates comments as a nested resource within articles. This is another part of capturing the hierarchical relationship that exists between articles and comments.
+    resources :comments
+  end
 end
